35a0561dc075ec67a275c2c25d01f77ba99c8c25
gp <giulio.pietroiusti@gmail.com>
2022-08-15 11:00:05 +0200

    Add messages service.
    Inject it in the hero service (service-in-service) and use it to add a
    message when we fetch the list of heroes with getHeroes().
    
    Inject it in the heroes component and use it to add a message when a
    hero is selected.
    
    Inject it as a public property in the messages component so that --
    besides displaying the list of messages --- messageService.clear() is
    called when a button in the template is clicked.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,20 +9,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  constructor(private heroService: HeroService) {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
