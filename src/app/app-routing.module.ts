cbd693d0c28f0069157e49e581fc271e3cbafa81
pietroiusti <giulio.pietroiusti@gmail.com>
2022-08-16 09:59:23 +0200

    Add routing and navigation link to list of heroes.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], //add to imports & configure
+  exports: [RouterModule], // make it available
+})
+export class AppRoutingModule { }
